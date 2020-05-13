aa117de6089dc1a18b4c9408842dcd697bbf79a9
Luuk de Vlieger <info@luuk.site>
2020-05-13 11:09:48 +0200

    :sparkles: Docs: wrap component

---
+++ docs/src/components/wrap.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default ({ children }: { children: React.ReactNode }) => (
+  <div
+    className={`
+      mx-auto
+      px-5
+      sm:px-10
+      max-w-screen-lg
+    `}
+  >
+    {children}
+  </div>
+)
