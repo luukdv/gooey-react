ee03fff98a16df2bf363b07567dc0270ea77a4d4
Luuk de Vlieger <info@luuk.site>
2020-05-13 16:59:36 +0200

    :sparkles: Add 'intensity' prop

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,49 @@
 import React, { Fragment, ReactNode } from 'react'
 
-const shift = 64
-
 const Goo = ({
-  children
+  children,
+  intensity = 'medium',
 }: {
-  children: ReactNode
-}) => (
-  <Fragment>
-    <svg
-      style={{
-        pointerEvents: 'none',
-        position: 'absolute',
-      }}
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <defs>
-        <filter id="gooey-react">
-          <feGaussianBlur
-            stdDeviation="12"
-          />
-          <feColorMatrix
-            values={`
-              1 0 0 0 0
-              0 1 0 0 0
-              0 0 1 0 0
-              0 0 0 ${shift} ${shift / -2}
-            `}
-          />
-        </filter>
-      </defs>
-    </svg>
-    <div
-      style={{
-        filter: 'url(#gooey-react)',
-      }}
-    >
-      {children}
-    </div>
-  </Fragment>
-)
+  children: ReactNode;
+  intensity?: 'weak' | 'medium' | 'strong';
+}) => {
+  const blur = intensity === 'weak' ? 8 : intensity === 'strong' ? 16 : 12
+  const shift = blur * 6
+
+  return (
+    <Fragment>
+      <svg
+        style={{
+          pointerEvents: 'none',
+          position: 'absolute',
+        }}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <defs>
+          <filter id="gooey-react">
+            <feGaussianBlur
+              stdDeviation={blur}
+            />
+            <feColorMatrix
+              values={`
+                1 0 0 0 0
+                0 1 0 0 0
+                0 0 1 0 0
+                0 0 0 ${shift} ${shift / -2}
+              `}
+            />
+          </filter>
+        </defs>
+      </svg>
+      <div
+        style={{
+          filter: 'url(#gooey-react)',
+        }}
+      >
+        {children}
+      </div>
+    </Fragment>
+  )
+}
 
 export default Goo
