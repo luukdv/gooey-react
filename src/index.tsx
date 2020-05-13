50c5b2d01ee3e71ea2966ea8601eaa49b7b9ad30
Luuk de Vlieger <info@luuk.site>
2020-05-13 16:30:08 +0200

    :construction: Component

---
+++ src/index.tsx
@@ -0,0 +1,44 @@
+import React, { Fragment, ReactNode } from 'react'
+
+const shift = 80
+
+const Goo = ({
+  children
+}: {
+  children: ReactNode
+}) => (
+  <Fragment>
+    <svg
+      style={{
+        pointerEvents: 'none',
+        position: 'absolute',
+      }}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <defs>
+        <filter id="gooey-react">
+          <feGaussianBlur
+            stdDeviation="12"
+          />
+          <feColorMatrix
+            values={`
+              1 0 0 0 0
+              0 1 0 0 0
+              0 0 1 0 0
+              0 0 0 ${shift} -${shift / 2}
+            `}
+          />
+        </filter>
+      </defs>
+    </svg>
+    <div
+      style={{
+        filter: 'url(#gooey-react)',
+      }}
+    >
+      {children}
+    </div>
+  </Fragment>
+)
+
+export default Goo
