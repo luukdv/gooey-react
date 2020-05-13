300d9e9d2ab9f998f63466c23b97658b3f94d726
Luuk de Vlieger <info@luuk.site>
2020-05-13 11:10:15 +0200

    :construction: Docs: pages

---
+++ docs/src/pages/404.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Wrap from '../components/wrap'
+
+export default () => (
+  <Wrap>
+    <h1>404</h1>
+  </Wrap>
+)
---
+++ docs/src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Wrap from '../components/wrap'
+
+export default () => (
+  <Wrap>
+    <h1>The gooey effect for React</h1>
+  </Wrap>
+)
