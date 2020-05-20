a7f5c386076efb9acb62cb81abf22e1a349ef241
Luuk de Vlieger <info@luuk.site>
2020-05-20 14:46:39 +0200

    :construction: Docs intro

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Goo from '../../../src'
 import Wrap from '../components/wrap'
 
 export default () => (
   <Wrap>
+    <Goo intensity="strong" className="flex w-full items-center justify-center relative" style={{
+        height: '50vh'
+      }}>
+      <div className="w-32 h-32 rounded-full absolute" style={{
+        background: 'linear-gradient(to top, darkviolet, darkturquoise)'
+      }} />
+      <div className="w-16 h-16 ml-16 rounded-full absolute" style={{
+        background: 'linear-gradient(to top, darkviolet, darkturquoise)'
+      }} />
+      <div className="w-48 h-48 mt-32 rounded-full absolute" style={{
+        background: 'linear-gradient(to top, darkviolet, darkturquoise)'
+      }} />
+    </Goo>
     <h1
       className={`
         leading-tight
@@ -17,11 +31,10 @@ export default () => (
     >
       The gooey effect for React
     </h1>
-    <p className="max-w-2xl leading-loose text-cool-gray-600 text-lg">
+    <p className="max-w-2xl mt-8 leading-loose text-cool-gray-600 text-lg">
       A tiny package for applying the gooey effect to React components. It's
       optimized to be as sharp/crisp as possible (existing implementations can
-      be a bit blurry). It's easy to use, does as little as possible and has a
-      footprint of just ~1 KB.
+      be a bit blurry). It's easy to use and has a footprint of just ~1 KB.
     </p>
   </Wrap>
 )
