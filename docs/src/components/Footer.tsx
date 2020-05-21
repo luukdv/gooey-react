0f8a42c681847eabf023dfb74029a35c26997108
Luuk de Vlieger <info@luuk.site>
2020-05-21 10:29:46 +0200

    :construction: Add footer

---
+++ docs/src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Wrap from './Wrap'
+
+export default () => (
+  <div className="bg-cool-gray-100 w-full text-cool-gray-600 text-sm mt-16">
+    <Wrap>
+      <p className="text-left py-16">
+        gooey-react is a project by{' '}
+        <a
+          className="underline font-medium"
+          href="https://www.luuk.site"
+          target="_blank"
+          rel="noopener"
+        >
+          Luuk
+        </a>
+        .
+      </p>
+    </Wrap>
+  </div>
+)
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,46 +1,54 @@
 import React from 'react'
 import Wrap from '../components/Wrap'
+import Footer from '../components/Footer'
 import Hero from '../components/Hero'
 
 export default () => (
-  <Wrap>
-    <Hero />
-    <h1
-      className={`
-        leading-tight
-        text-4xl
-        md:text-5xl
-        lg:text-6xl
-        tracking-tighter
-        text-cool-gray-800
-        font-bold
-        font-heading
-      `}
-    >
-      The gooey effect for React
-    </h1>
-    <p className="max-w-2xl mt-8 leading-loose text-cool-gray-600 text-lg mb-16">
-      A tiny package for applying the gooey effect to React components. It's
-      optimized to be as sharp/crisp as possible (existing implementations can
-      be a bit blurry). It's easy to use and has a footprint of just ~1 KB.
-    </p>
-    <div className="w-full h-px bg-cool-gray-200 mb-16" />
-    <h2
-      className={`
-        leading-tight
-        text-2xl
-        md:text-34xl
-        lg:text-4xl
-        tracking-tighter
-        text-cool-gray-800
-        font-bold
-        font-heading
-      `}
-    >
-      Some examples
-    </h2>
-    <p className="max-w-2xl mt-8 leading-loose text-cool-gray-600 text-base mb-16">
-      
-    </p>
-  </Wrap>
+  <>
+    <Wrap>
+      <Hero />
+      <h1
+        className={`
+          leading-tight
+          text-4xl
+          md:text-5xl
+          lg:text-6xl
+          tracking-tighter
+          text-cool-gray-800
+          font-bold
+          font-heading
+        `}
+      >
+        The gooey effect for React
+      </h1>
+      <p className="max-w-2xl mt-8 leading-loose text-lg mb-16">
+        The 'gooey effect' has been made popular by various (amazing) blogposts
+        over the years. This tiny package makes it easy to use within React, and
+        has improved the implementation as much as possible. It's optimized to
+        be as sharp/crisp as possible, since existing implementations can be a
+        bit blurry. Support for Safari (which can be notorious regarding SVG
+        filter animations) is added as well.
+      </p>
+      <div className="w-full h-px bg-cool-gray-200 mb-16" />
+      <h2
+        className={`
+          leading-tight
+          text-2xl
+          md:text-3xl
+          lg:text-4xl
+          tracking-tighter
+          text-cool-gray-800
+          font-bold
+          font-heading
+          mb-8
+        `}
+      >
+        Installation (1 kb)
+      </h2>
+      <code className="bg-orange-100 px-12 py-6 text-cool-gray-700 rounded-lg text-lg mb-16">
+        npm install gooey-react
+      </code>
+    </Wrap>
+    <Footer />
+  </>
 )
