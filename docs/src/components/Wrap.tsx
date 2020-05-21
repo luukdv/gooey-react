af78398a25234add814abb51db8abadc01f46164
Luuk de Vlieger <info@luuk.site>
2020-05-21 09:01:34 +0200

    :art: Add camelcase wrap

---
+++ docs/src/components/Wrap.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default ({ children }: { children: React.ReactNode }) => (
+  <div
+    className={`
+      mx-auto
+      px-5
+      sm:px-10
+      max-w-screen-lg
+      flex
+      flex-col
+      items-center
+    `}
+  >
+    {children}
+  </div>
+)
