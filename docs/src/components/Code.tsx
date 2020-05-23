0178be9d137efb6c73110df1fa4c378f84b70178
Luuk de Vlieger <info@luuk.site>
2020-05-23 18:35:28 +0200

    :lipstick: Center align code

--- docs/src/components/Code.tsx
+++ docs/src/components/Code.tsx
@@ -14,7 +14,7 @@ export default ({
       py-6
       text-cool-gray-700
       rounded-lg
-      text-left
+      ${large ? 'text-center' : 'text-left'}
       ${large ? 'text-lg' : 'text-sm'}
       ${large ? 'mb-16' : 'mb-8'}
     `}
