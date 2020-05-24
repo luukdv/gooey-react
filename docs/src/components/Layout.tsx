e7859ca99db321e0944735f497659ed2fb560fe0
Luuk de Vlieger <info@luuk.site>
2020-05-24 15:56:29 +0200

    :truck: Extract sidebar component

--- docs/src/components/Layout.tsx
+++ docs/src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Wrap from '../components/Wrap'
 import Footer from '../components/Footer'
+import Sidebar from '../components/Sidebar'
 
 export default ({
   children,
@@ -12,28 +13,7 @@ export default ({
   <div className={`flex flex-col ${home ? '' : 'min-h-screen'}`}>
     <Wrap noMargin={home}>
       <div className="flex flex-col md:flex-row w-full">
-        <div
-          className="
-            mt-8
-            md:mt-0
-            w-full
-            md:w-1/4
-            lg:w-1/5
-            flex-none
-            mr-8
-            lg:mr-16
-            order-last
-            md:order-first
-          "
-        >
-          <div className="bg-gray-100 rounded-md md:sticky md:top-8 md:mt-2">
-            Home
-            <ul>
-              <li>Installation</li>
-              <li>Usage</li>
-            </ul>
-          </div>
-        </div>
+        <Sidebar />
         <div className="flex-grow flex flex-col items-start main">{children}</div>
       </div>
     </Wrap>
---
+++ docs/src/components/Sidebar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default () => (
+  <div
+    className="
+      mt-8
+      md:mt-0
+      w-full
+      md:w-1/4
+      lg:w-1/5
+      flex-none
+      mr-8
+      lg:mr-16
+      order-last
+      md:order-first
+    "
+  >
+    <div className="bg-gray-100 rounded-md md:sticky md:top-8 md:mt-2">
+      Home
+      <ul>
+        <li>Installation</li>
+        <li>Usage</li>
+      </ul>
+    </div>
+  </div>
+)
