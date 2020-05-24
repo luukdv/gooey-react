d324386d2e5011e5ce08475644fd0405fc7a9bfd
Luuk de Vlieger <info@luuk.site>
2020-05-24 11:14:02 +0200

    :sparkles: Docs: add section about 'intensity' prop

--- docs/src/components/Properties.tsx
+++ docs/src/components/Properties.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Goo from '../../../src'
 import H2 from './H2'
 import P from './P'
 import Code from './Code'
@@ -9,23 +10,85 @@ export default () => (
     <div className="w-full h-px bg-cool-gray-200 mb-16" />
     <H2>Properties</H2>
     <P>
-      Asides from the possibility to add a <Tag>className</Tag> and{' '}
-      <Tag>style</Tag> prop, the only available property is <Tag>intensity</Tag>
-      . You can use this to control how strong the gooey effect is applied. It
-      has three possible values: <Tag>weak</Tag>, <Tag>medium</Tag> (which is
-      the default) and <Tag>strong</Tag>.
+      You can use <Tag>intensity</Tag> to control how strong the gooey effect is
+      applied. It has three possible values: <Tag>weak</Tag>, <Tag>medium</Tag>{' '}
+      (which is the default) and <Tag>strong</Tag>.
     </P>
-    <Code>
-      <span className="text-blue-700">{'<'}</span>
-      <span className="text-yellow-700">{'Goo '}</span>
-      <span className="text-purple-700">{'intensity'}</span>
-      <span className="text-blue-700">{'="'}</span>
-      <span className="text-green-700">{'weak'}</span>
-      <span className="text-blue-700">{'">'}</span>
-      <span className="text-cool-gray-500"> … </span>
-      <span className="text-blue-700">{'</'}</span>
-      <span className="text-yellow-700">{'Goo'}</span>
-      <span className="text-blue-700">{'>'}</span>
-    </Code>
+    <div className="md:grid md:grid-cols-2 md:gap-8 w-full">
+      <div className="flex flex-col items-center">
+        <Code>
+          <span className="text-blue-700">{'<'}</span>
+          <span className="text-yellow-700">{'Goo '}</span>
+          <span className="text-purple-700">{'intensity'}</span>
+          <span className="text-blue-700">{'="'}</span>
+          <span className="text-green-700">{'weak'}</span>
+          <span className="text-blue-700">{'">'}</span>
+          <span className="text-cool-gray-500"> … </span>
+          <span className="text-blue-700">{'</'}</span>
+          <span className="text-yellow-700">{'Goo'}</span>
+          <span className="text-blue-700">{'>'}</span>
+        </Code>
+        <div className="border-2 border-cool-gray-200 w-full">
+          <Goo intensity="weak" id="prop-weak">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-full h-64">
+              <circle
+                cx="50%"
+                cy="31.25%"
+                r="30"
+                fill="salmon"
+                className="origin-center"
+                style={{
+                  animation: 'drop 2s ease-in infinite alternate',
+                }}
+              />
+              <circle
+                cx="50%"
+                cy="31.25%"
+                r="48"
+                fill="lightsalmon"
+                className="origin-center"
+              />
+            </svg>
+          </Goo>
+        </div>
+      </div>
+      <div className="flex flex-col items-center">
+        <Code>
+          <span className="text-blue-700">{'<'}</span>
+          <span className="text-yellow-700">{'Goo '}</span>
+          <span className="text-purple-700">{'intensity'}</span>
+          <span className="text-blue-700">{'="'}</span>
+          <span className="text-green-700">{'strong'}</span>
+          <span className="text-blue-700">{'">'}</span>
+          <span className="text-cool-gray-500"> … </span>
+          <span className="text-blue-700">{'</'}</span>
+          <span className="text-yellow-700">{'Goo'}</span>
+          <span className="text-blue-700">{'>'}</span>
+        </Code>
+        <div className="border-2 border-cool-gray-200 w-full">
+          <Goo intensity="strong" id="prop-strong">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-full h-64">
+              <circle
+                cx="50%"
+                cy="31.25%"
+                r="30"
+                fill="salmon"
+                className="origin-center"
+                style={{
+                  animation: 'drop 2s ease-in infinite alternate',
+                }}
+              />
+              <circle
+                cx="50%"
+                cy="31.25%"
+                r="48"
+                fill="lightsalmon"
+                className="origin-center"
+              />
+            </svg>
+          </Goo>
+        </div>
+      </div>
+    </div>
   </>
 )
