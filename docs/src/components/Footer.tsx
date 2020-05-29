803d4e938fe9fea9d308f42948b90302a6b356d6
Luuk de Vlieger <info@luuk.site>
2020-05-29 13:34:06 +0200

    :sparkles: Add footer links

--- docs/src/components/Footer.tsx
+++ docs/src/components/Footer.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import Wrap from './Wrap'
 
 export default () => (
-  <div className="bg-cool-gray-100 w-full text-sm mt-8 md:mt-12 lg:mt-16">
+  <div className="bg-cool-gray-100 text-sm mt-8 md:mt-12 lg:mt-16 text-center">
     <Wrap noMargin>
-      <p className="text-center py-12 lg:py-16">
+      <div className="flex flex-col sm:flex-row justify-between py-12 lg:py-16">
+      <p className="mb-8 sm:mb-0">
         gooey-react is a project by{' '}
         <a
           className="underline font-medium"
@@ -16,6 +17,33 @@ export default () => (
         </a>
         .
       </p>
+      <div className="flex justify-center">
+      <a
+        className="underline font-semibold px-4"
+        href="https://www.npmjs.com/package/gooey-react"
+        target="_blank"
+        rel="nofollow noopener noreferrer"
+      >
+        NPM
+      </a>
+      <a
+        className="underline font-semibold px-4"
+        href="https://github.com/luukdv/gooey-react"
+        target="_blank"
+        rel="nofollow noopener noreferrer"
+      >
+        GitHub
+      </a>
+      <a
+        className="underline font-semibold px-4"
+        href="https://www.luuk.site"
+        target="_blank"
+        rel="noopener"
+      >
+        Contact
+      </a>
+    </div>
+      </div>
     </Wrap>
   </div>
 )
