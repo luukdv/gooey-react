5b1dc6b36e2068988699bca90f9ee54e1a327358
Luuk de Vlieger <info@luuk.site>
2020-05-29 16:04:57 +0200

    :speech_balloon: Browser support page

--- docs/src/pages/browser-support.tsx
+++ docs/src/pages/browser-support.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import H1 from '../components/H1'
 import Layout from '../components/Layout'
+import P from '../components/P'
 
 export default () => (
   <Layout>
     <H1>Browser support</H1>
+    <P>
+      This package is not supported in Internet Explorer. If you experience
+      problems in other browsers, make sure you're using an SVG instead of
+      regular HTML elements in the component.
+    </P>
   </Layout>
 )
