b246187664c0f325544455e2d10117e8149f0835
Luuk de Vlieger <info@luuk.site>
2020-05-29 16:16:20 +0200

    :sparkles: 404 page

--- docs/src/pages/404.tsx
+++ docs/src/pages/404.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
-import Wrap from '../components/Wrap'
+import Layout from '../components/Layout'
+import H1 from '../components/H1'
+import Button from '../components/Button'
+import Divider from '../components/Divider'
+import P from '../components/P'
 
 export default () => (
-  <Wrap>
-    <h1>404</h1>
-  </Wrap>
+  <Layout>
+    <H1>Not found</H1>
+    <P large>That's too bad. What brought you here?</P>
+    <Divider />
+    <Button href="/" title="Back to home" />
+  </Layout>
 )
