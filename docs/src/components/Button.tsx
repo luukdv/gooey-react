797fecd0ac9a32e63c41a01318a7ed418a2a8116
Luuk de Vlieger <info@luuk.site>
2020-05-29 16:16:10 +0200

    :lipstick: Button padding

--- docs/src/components/Button.tsx
+++ docs/src/components/Button.tsx
@@ -6,8 +6,8 @@ export default ({ href, title }: { href: string; title: string }) => (
     to={href}
     className={`
       text-white
-      px-4
-      py-2
+      px-5
+      py-3
       rounded
       shadow
       font-semibold
