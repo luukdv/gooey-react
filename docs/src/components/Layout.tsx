4c8d660d465de38a1a52ce62488635663e662f43
Luuk de Vlieger <info@luuk.site>
2020-05-29 15:35:28 +0200

    :bookmark: Bump version

--- docs/src/components/Layout.tsx
+++ docs/src/components/Layout.tsx
@@ -35,7 +35,7 @@ export default ({
             <div className="font-bold md:text-lg text-cool-gray-800 mr-2">
               gooey-react
             </div>
-            <div className="text-cool-gray-500">v0.3.0</div>
+            <div className="text-cool-gray-500">v0.4.0</div>
           </div>
           <a
             className="underline text-sm font-semibold text-cool-gray-800"
