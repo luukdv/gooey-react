465ac478d6850ea3f6b57fe4e8823ade18f51d84
Luuk de Vlieger <info@luuk.site>
2020-05-29 15:44:52 +0200

    :heavy_plus_sign: Analytics

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -2,11 +2,13 @@ module.exports = {
   plugins: [
     {
       resolve: 'gatsby-plugin-canonical-urls',
-      options: {
-        siteUrl: 'https://gooey-react.netlify.app',
-      },
+      options: { siteUrl: 'https://gooey-react.netlify.app' },
     },
     'gatsby-plugin-force-trailing-slashes',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: { trackingId: 'UA-75278199-3' },
+    },
     'gatsby-plugin-netlify',
     'gatsby-plugin-postcss',
     'gatsby-plugin-typescript',
