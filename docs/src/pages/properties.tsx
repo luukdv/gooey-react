2b3c1b554101fcfb5ea161a9fbb63a70f5583622
Luuk de Vlieger <info@luuk.site>
2020-06-01 11:10:49 +0200

    :mag: Title and description for props overview

--- docs/src/pages/properties.tsx
+++ docs/src/pages/properties.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import H1 from '../components/H1'
 import Layout from '../components/Layout'
+import P from '../components/P'
+import Tag from '../components/Tag'
 import Button from '../components/Button'
 
 export default () => (
@@ -9,8 +11,11 @@ export default () => (
     prevLabel="Home"
     next="/properties/intensity/"
     nextLabel="Prop: intensity"
+    title="Properties"
+    description="The Goo component has a few simple props available for fine-tuning the visual outcome of the filter."
   >
     <H1>Properties</H1>
+    <P large smallMargin>The <Tag>Goo</Tag> component has a few simple props available for fine-tuning the visual outcome of the filter.</P>
     <Button href="/properties/intensity/" title="intensity" />
     <Button href="/properties/composite/" title="composite" />
     <Button href="/properties/id/" title="id" />
