ca82271f2b59ec38f45d4f9167e06a1bcd1ab345
Luuk de Vlieger <info@luuk.site>
2020-06-06 16:57:01 +0200

    :sparkles: Add next step to tutorial

--- docs/src/pages/tutorial.tsx
+++ docs/src/pages/tutorial.tsx
@@ -6,6 +6,7 @@ import P from '../components/P'
 import Divider from '../components/Divider'
 import Code from '../components/Code'
 import Tag from '../components/Tag'
+import { Link } from 'gatsby'
 
 export default () => (
   <Layout
@@ -17,10 +18,10 @@ export default () => (
     description="Let's create something simple to show how to use this library."
   >
     <H1>Tutorial</H1>
-    <P smallMargin>Let's create something simple to show how to use this library. First, install the dependency with NPM or Yarn.</P>
+    <P smallMargin>Let's create something simple to show how this library can be used. First, install the dependency with NPM or Yarn.</P>
     <Code moreMargin>npm install gooey-react</Code>
     <Divider />
-    <P smallMargin>Next up, we'll add some elements inside the component. You can use regular HTML elements (like a <Tag>div</Tag>) as well, but here we'll use an SVG for optimal browser support.</P>
+    <P smallMargin>Next, we'll add some elements inside the component. You can use regular HTML elements (like a <Tag>div</Tag>) as well, but here an SVG is used for optimal browser support.</P>
     <Code moreMargin>
     <span className="text-blue-700">import</span> Goo{' '}
       <span className="text-blue-700">from '</span>
@@ -74,10 +75,10 @@ export default () => (
       <span className="text-red-700">{'circle '}</span>
       <span className="text-purple-700">{'cx'}</span>
       <span className="text-blue-700">{'="'}</span>
-      <span className="text-green-700">{'37% '}</span>
+      <span className="text-green-700">{'34% '}</span>
       <span className="text-purple-700">{'cy'}</span>
       <span className="text-blue-700">{'="'}</span>
-      <span className="text-green-700">{'37% '}</span>
+      <span className="text-green-700">{'34% '}</span>
       <span className="text-purple-700">{'fill'}</span>
       <span className="text-blue-700">{'="'}</span>
       <span className="text-green-700">{'orchid '}</span>
@@ -91,10 +92,10 @@ export default () => (
       <span className="text-red-700">{'circle '}</span>
       <span className="text-purple-700">{'cx'}</span>
       <span className="text-blue-700">{'="'}</span>
-      <span className="text-green-700">{'63% '}</span>
+      <span className="text-green-700">{'66% '}</span>
       <span className="text-purple-700">{'cy'}</span>
       <span className="text-blue-700">{'="'}</span>
-      <span className="text-green-700">{'63% '}</span>
+      <span className="text-green-700">{'66% '}</span>
       <span className="text-purple-700">{'fill'}</span>
       <span className="text-blue-700">{'="'}</span>
       <span className="text-green-700">{'orchid '}</span>
@@ -106,9 +107,8 @@ export default () => (
       <span className="text-blue-700">{'</'}</span>
       <span className="text-red-700">{'svg'}</span>
       <span className="text-blue-700">{'>'}</span>
-      <br />
     </Code>
-    <P smallMargin>Which will look like the following:</P>
+    <P smallMargin>Which will look like the following.</P>
     <div className="border-2 border-cool-gray-200 mb-8 md:mb-12">
       <Goo>
         <svg width="192" height="192">
@@ -118,5 +118,35 @@ export default () => (
       </Goo>
     </div>
     <Divider />
+    <P smallMargin>If we move the elements closer together, the effect becomes apparent. You can use the <Link className="underline text-purple-600 hover:text-pink-600" to="/properties/intensity/">intensity</Link> prop to control how strong the goo is applied.</P>
+    <Code>
+      <span className="text-blue-700">{'<'}</span>
+      <span className="text-red-700">{'circle '}</span>
+      <span className="text-purple-700">{'cx'}</span>
+      <span className="text-blue-700">{'="'}</span>
+      <span className="text-green-700">{'37% '}</span>
+      <span className="text-purple-700">{'cy'}</span>
+      <span className="text-blue-700">{'="'}</span>
+      <span className="text-green-700">{'37%'}</span>
+      <span className="text-blue-700">{'">'}</span>
+      <br />
+      <span className="text-blue-700">{'<'}</span>
+      <span className="text-red-700">{'circle '}</span>
+      <span className="text-purple-700">{'cx'}</span>
+      <span className="text-blue-700">{'="'}</span>
+      <span className="text-green-700">{'63% '}</span>
+      <span className="text-purple-700">{'cy'}</span>
+      <span className="text-blue-700">{'="'}</span>
+      <span className="text-green-700">{'63%'}</span>
+      <span className="text-blue-700">{'">'}</span>
+    </Code>
+    <div className="border-2 border-cool-gray-200">
+      <Goo>
+        <svg width="192" height="192">
+          <circle cx="37%" cy="37%" fill="orchid" r="32" />
+          <circle cx="63%" cy="63%" fill="orchid" r="32" />
+        </svg>
+      </Goo>
+    </div>
   </Layout>
 )
