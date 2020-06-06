1b922e03b9bcdf5b7e0ee06a37a689e2a1f29260
Luuk de Vlieger <info@luuk.site>
2020-06-06 16:30:25 +0200

    :sparkles: First steps of tutorial

--- docs/src/pages/tutorial.tsx
+++ docs/src/pages/tutorial.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Goo from 'gooey-react'
 import H1 from '../components/H1'
 import Layout from '../components/Layout'
 import P from '../components/P'
+import Divider from '../components/Divider'
+import Code from '../components/Code'
+import Tag from '../components/Tag'
 
 export default () => (
   <Layout
@@ -10,11 +14,47 @@ export default () => (
     next="/examples/around-the-web/"
     nextLabel="Examples: around the web"
     title="Tutorial"
-    description="You can forward a className and/or style property to the component, which will be applied to the top-level element."
+    description="Let's create something simple to show how to use this library."
   >
     <H1>Tutorial</H1>
-    <P large>
-      Work in progress… Please come back later.
-    </P>
+    <P smallMargin>Let's create something simple to show how to use this library. First, install the dependency with NPM or Yarn.</P>
+    <Code moreMargin>npm install gooey-react</Code>
+    <Divider />
+    <P smallMargin>Next up, we'll add some elements inside the component. You can use regular HTML elements (like <Tag>div</Tag>s) as well, but here we'll use an SVG for optimal browser support.</P>
+    <Code moreMargin>
+    <span className="text-blue-700">import</span> Goo{' '}
+      <span className="text-blue-700">from '</span>
+      <span className="text-green-700">gooey-react</span>
+      <span className="text-blue-700">'</span>
+      <br />
+      <br />
+      <span className="text-blue-700">{'<'}</span>
+      <span className="text-yellow-700">{'Goo'}</span>
+      <span className="text-blue-700">{'>'}</span>
+      <br />
+      &nbsp;&nbsp;
+      <span className="text-blue-700">{'<'}</span>
+      <span className="text-red-700">{'svg'}</span>
+      <span className="text-blue-700">{'>'}</span>
+      <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <span className="text-blue-700">{'<'}</span>
+      <span className="text-red-700">{'circle'}</span>
+      <span className="text-blue-700">{' />'}</span>
+      <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <span className="text-blue-700">{'<'}</span>
+      <span className="text-red-700">{'circle'}</span>
+      <span className="text-blue-700">{' />'}</span>
+      <br />
+      &nbsp;&nbsp;
+      <span className="text-blue-700">{'</'}</span>
+      <span className="text-red-700">{'svg'}</span>
+      <span className="text-blue-700">{'>'}</span>
+      <br />
+      <span className="text-blue-700">{'</'}</span>
+      <span className="text-yellow-700">{'Goo'}</span>
+      <span className="text-blue-700">{'>'}</span>
+    </Code>
   </Layout>
 )
