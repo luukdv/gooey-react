46d4d9409b74e65eb702903f8f6d66530948836c
Luuk de Vlieger <info@luuk.site>
2020-10-05 12:07:06 +0200

    :speech_balloon: Update docs: browser support

--- docs/src/pages/browser-support.tsx
+++ docs/src/pages/browser-support.tsx
@@ -14,18 +14,10 @@ export default () => (
   >
     <H1>Browser support</H1>
     <P large smallMargin>
-      This package is not supported in Internet Explorer. If you experience
-      problems in other browsers, make sure you're using an SVG instead of
-      regular HTML elements inside the component.
-    </P>
-    <P large smallMargin>
-      iOS has a few bugs regarding SVG filters. If you're having problems on
-      iOS, you can apply the following changes:
+      This package is not supported in Internet Explorer. If you're having problems on iOS (which has a few bugs regarding SVG filters), you can apply the following changes:
     </P>
     <List
       items={[
-        'Use an SVG instead of regular HTML elements',
-        'Put elements inside the SVG in a group',
         'Make sure there is enough extra space on all sides inside the SVG',
         'Animate all elements (instead of just a selection) to reduce artifacts',
       ]}
